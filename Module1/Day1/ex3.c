51588194c2e885e74ae7398055a6006cef81e685
raunak541 <134433127+raunak541@users.noreply.github.com>
2023-06-15 15:04:45 +0530

    Add files via upload

---
+++ Module1/Day1/ex3.c
@@ -0,0 +1,36 @@
+//#include <stdio.h>
+
+int main() {
+    int rollNo;
+    char name[100];
+    float physicsMarks, mathMarks, chemistryMarks;
+    float totalMarks, percentage;
+
+    // Read roll number, name, and marks
+    printf("Enter Roll Number: ");
+    scanf("%d", &rollNo);
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", name);
+
+    printf("Enter Marks of Physics: ");
+    scanf("%f", &physicsMarks);
+
+    printf("Enter Marks of Math: ");
+    scanf("%f", &mathMarks);
+
+    printf("Enter Marks of Chemistry: ");
+    scanf("%f", &chemistryMarks);
+
+    // Calculate total marks and percentage
+    totalMarks = physicsMarks + mathMarks + chemistryMarks;
+    percentage = (totalMarks / 300) * 100;
+
+    // Print the summary
+    printf("\n---- Student Summary ----\n");
+    printf("Roll Number: %d\nName: %s\n", rollNo, name);
+    printf("Physics Marks: %.2f\nMath Marks: %.2f\nChemistry Marks: %.2f\n", physicsMarks, mathMarks, chemistryMarks);
+    printf("Total Marks: %.2f\nPercentage: %.2f%%\n", totalMarks, percentage);
+
+    return 0;
+}
---
+++ Module1/Day1/ex4.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char operator;
+
+    printf("Enter Number 1, Operator, Number 2: ");
+    scanf("%f %c %f", &num1, &operator, &num2);
+
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = (num2 != 0) ? (num1 / num2) : 0; break;
+        default:
+            printf("Error: Invalid operator.\n");
+            return 1;
+    }
+
+    printf("Result: %.2f\n", result);
+
+    return 0;
+}
